c299cffb8f575924711c4aa369eb7efc4073947f
Mensah Kwame Anni <mkanni@st.ug.edu.gh>
2024-12-19 01:00:18 +0000

    initial commit

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -9,6 +9,26 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+       Library library = new Library();
+
+       //adding a book 
+       library.addBook(new Book("2024","Mensah Anni","1234567891023"));
+       library.addBook(new Book("Java Made simple","Mensah Anni","098765432123"));
+       library.addBook(new Book("Code With Me","Mensah Anni","675894032123"));
+
+       //adding the patrons
+       Patron patron1 = new Patron("Dr Nassy", "P001");
+       Patron patron2 = new Patron("Mr Green", "P002");
+
+       library.addPatron(patron1);
+       library.addPatron(patron2);
+
+       //operartions
+       library.displayAvailableBooks();
+       library.checkoutBook("1234567891023", patron1);
+       library.displayAvailableBooks();
+       library.returnBook("1234567891023");
+       library.displayAvailableBooks();
     }
+
 }
--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,5 +1,34 @@
 public class Book {
+// This is your working class.
 
-    // This is your working class.
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isAvailable;
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isAvailable = true; //this tells that the book is always avilable
+    }
+
+    //assigning getters
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
     
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean avilable) {
+        isAvailable = avilable;
+    }
 }
